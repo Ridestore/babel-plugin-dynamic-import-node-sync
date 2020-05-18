7317dd6b2f2de5d3ddc73d1e795865a74ec822f2
Ivan Maslov <mail@studentivan.ru>
2020-05-18 18:31:11 +0300

    Add babel v7 support

--- src/index.js
+++ src/index.js
@@ -5,43 +5,38 @@ export default function ({ template, types: t }) {
     const r = require(SOURCE);r.then = cb => cb(r);return r;
   `);
 
-  const build = (path, importArguments) => path.replaceWithMultiple(buildImport({
-    SOURCE: (t.isStringLiteral(importArguments[0]) || t.isTemplateLiteral(importArguments[0]))
+  const build = (path, importArguments) => {
+    const isString = t.isStringLiteral(importArguments[0]) || t.isTemplateLiteral(importArguments[0]);
+
+    if (isString) {
+      t.removeComments(importArguments[0]);
+    }
+    const newImport = buildImport({
+      SOURCE: (isString)
         ? importArguments
-        : t.templateLiteral([t.templateElement({ raw: '' }), t.templateElement({ raw: '' }, true)], importArguments),
-  }));
+        : t.templateLiteral([
+          t.templateElement({ raw: '', cooked: '' }),
+          t.templateElement({ raw: '', cooked: '' }, true),
+        ], importArguments),
+    });
+    path.parentPath.replaceWithMultiple(newImport);
+  };
 
   return {
     inherits: syntax,
     visitor: {
       Import(path, { opts }) {
         const importArguments = path.parentPath.node.arguments;
-        const importArgument = importArguments[0];
 
         if (opts && !opts.target) {
           return build(path, importArguments);
         }
 
-        (importArgument.leadingComments || [])
+        (importArguments[0].leadingComments || [])
           // eslint-disable-next-line max-len
           .filter(comment => comment && comment.value && comment.value.trim().toLowerCase() === opts.target)
           .slice(0, 1)
           .forEach(() => build(path, importArguments));
-
-        // const isString = t.isStringLiteral(importArgument) || t.isTemplateLiteral(importArgument);
-
-        // if (isString) {
-        //   t.removeComments(importArguments[0]);
-        // }
-        // const newImport = buildImport({
-        //   SOURCE: (isString)
-        //     ? importArguments
-        //     : t.templateLiteral([
-        //       t.templateElement({ raw: '', cooked: '' }),
-        //       t.templateElement({ raw: '', cooked: '' }, true),
-        //     ], importArguments),
-        // });
-        // path.parentPath.replaceWith(newImport);
       },
     },
   };
